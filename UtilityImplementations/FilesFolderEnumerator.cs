b00be6d8748004695f3520d303278c8e1c037c80
kenttc <kenttc@gmail.com>
2015-11-24 17:06:06 +0000

    added new folder file enumerator

---
+++ UtilityImplementations/FilesFolderEnumerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilityDomain.Interface;
+
+namespace UtilityImplementations
+{
+    public class FilesFolderEnumerator : IFilesFoldersEnumerator
+    {
+        public List<DirectoryInfo> GetDirectories(string rootDirectory)
+        {
+            if (rootDirectory.Length > 248) return new List<DirectoryInfo>();
+            var subDirectories = Directory.EnumerateDirectories(rootDirectory).ToList();
+            var result = new List<DirectoryInfo>();
+            subDirectories.ForEach(dir =>
+            {
+                result.Add(new System.IO.DirectoryInfo(dir));
+                result.AddRange(GetDirectories(dir));
+            });
+            return result;
+        }
+
+        public List<FileInfo> GetFiles(string directoryPath)
+        {
+            if (directoryPath.Length > 248) return new List<FileInfo>();
+            
+            return Directory.EnumerateFiles(directoryPath).Select(x =>new FileInfo(x)).ToList();
+
+        }
+    }
+}
---
+++ UtilityPOCOs/Interface/IFilesFoldersEnumerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityDomain.Interface
+{
+   public interface IFilesFoldersEnumerator
+   {
+
+       List<System.IO.DirectoryInfo> GetDirectories(string rootDirectory);
+       
+       List<System.IO.FileInfo> GetFiles(string directoryPath);
+
+   }
+}
